56afeea46e9b6d2899af9c2d69b496cde4704e4e
Fredrik Göransson <fredrik.goransson@gmail.com>
2013-08-20 18:36:11 +0200

    Initial Commit

---
+++ TestCocos2dNinjas/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TestCocos2dNinjas
+//
+//  Created by Fredrik Göransson on 8/20/13.
+//  Copyright Fredrik Göransson 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
